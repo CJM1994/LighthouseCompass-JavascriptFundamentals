c037090df4b06b0413bfbd7424c0b379343f1350
CJM1994 <connorjohnmullin@gmail.com>
2021-12-06 06:11:18 +0000

    whichschool exercise

---
+++ whichschool.js
@@ -0,0 +1,13 @@
+const whichSchool = function (age) {
+
+  if (age < 13) {
+    return 'Elementary School';
+  }
+  else if (13 <= age && age <= 18) {
+    return 'Secondary School';
+  }
+  else { return 'Lighthouse Labs'; }
+
+}
+
+console.log(whichSchool(44));
