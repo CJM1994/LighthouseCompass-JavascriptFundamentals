d7a0e42aa7badaca29d78e1e815d3778b4ce2661
CJM1994 <connorjohnmullin@gmail.com>
2021-12-09 06:16:06 +0000

    completed choosestations assignment

---
+++ choosestations.js
@@ -0,0 +1,20 @@
+// Returns all appropriate voting centres
+// Stations must have capacity of at least 20 and be a school or community centre
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+function chooseStations(stations) {
+  const goodStations = [];
+  for (let i = 0; i < stations.length; i++) {
+    if (stations[i][1] >= 20 && (stations[i][2] === 'school' || stations[i][2] === 'community centre')) {
+      goodStations.push(stations[i][0]);
+    }
+  }
+  return goodStations;
+}
+
+console.log(chooseStations(stations));
